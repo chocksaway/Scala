5c8d2f0d784da3c35682920dde95e61967c98aab
chocksaway <milesd@chocksaway.com>
2015-06-02 17:05:18 +0100

    Minor change.

--- src/com/chocksaway/future/FutureScanner.scala
+++ src/com/chocksaway/future/FutureScanner.scala
@@ -1,10 +1,12 @@
 package com.chocksaway.future
 
+import java.util
+
 import scala.concurrent._
 import ExecutionContext.Implicits.global
 
 import com.chocksaway.Portscanner
-import scala.util.{Failure, Success}
+import scala.util.Success
 import java.util.Date
 
 /**
@@ -14,7 +16,7 @@ import java.util.Date
 object FutureScanner {
     implicit class ForeachAsync[T](iterable: Iterable[T]) {
         def foreachAsync[U](f: T => U)(implicit ec: ExecutionContext): Unit = {
-            def next(i: Iterator[T]): Unit = if (i.hasNext) Future(f(i.next)) onComplete { case _ => next(i) }
+            def next(i: util.Iterator[T]): Unit = if (i.hasNext) Future(f(i.next)) onComplete { case _ => next(i) }
             next(iterable.iterator)
         }
     }
